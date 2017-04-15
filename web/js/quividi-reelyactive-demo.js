ad2ea1f2f2fe04e3061a5d3f14439ec9af7ab38c
jeffyactive <jeff@reelyactive.com>
2017-04-15 11:47:11 -0400

    First fully-baked demo

--- web/js/quividi-reelyactive-demo.js
+++ web/js/quividi-reelyactive-demo.js
@@ -3,7 +3,13 @@
  * We believe in an open Internet of Things
  */
 
-angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io' ])
+
+SNIFFYPEDIA_ROOT = 'https://sniffypedia.org/';
+DEFAULT_STORY = { image: 'https://sniffypedia.org/Organization/Bluetooth_SIG_Inc/400x400.png' };
+
+
+angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io',
+                         'reelyactive.cormorant' ])
 
   // socket.io connection for real-time analytics
   .factory('Socket', function(socketFactory, $location) {
@@ -14,7 +20,9 @@ angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io' ])
 
 
   // Recognition controller
-  .controller('RecognitionCtrl', function($scope, $http, $interval, Socket) {
+  .controller('RecognitionCtrl', function($scope, $http, $interval, Socket,
+                                          cormorant) {
+    $scope.presentStory = DEFAULT_STORY;
 
     Socket.on('radio-presence', function(event) {
       handleRadioPresenceEvent(event);
@@ -26,7 +34,14 @@ angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io' ])
 
     // Handle a radio presence event
     function handleRadioPresenceEvent(event) {
+      event.presence = Math.round((event.time - event.startTime) / 1000);
       event.associations = makeDeviceAssociations(event.deviceAssociationIds);
+      if(event.associations.length > 0) {
+        fetchStory(event.associations[0]);
+      }
+      else {
+        $scope.presentStory = DEFAULT_STORY;
+      }
       $scope.radioAnalytics = event;
     }
 
@@ -35,6 +50,7 @@ angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io' ])
       event.presence = Math.round(event.duration);
       event.attention = Math.round(100 * event.attention_time / event.duration);
       event.watching = ((parseInt(event.status) & 0x20) === 0x20);
+      event.image = getVideoPresenceImage(event);
       $scope.videoAnalytics = event;
     }
 
@@ -56,8 +72,48 @@ angular.module('demo', [ 'ui.bootstrap', 'btford.socket-io' ])
           associations.push(sniffypedia_index.ble.iBeacons[id]);
         }
       }
-      console.log(associations);
       return associations;
+    }
+
+    // Fetch the story of the given association
+    function fetchStory(association) {
+      var url = SNIFFYPEDIA_ROOT + association;
+      cormorant.getStory(url, function(story) {
+        $scope.presentStory =  { json: story };
+        if(story.hasOwnProperty('@graph')) {
+          $scope.presentStory.name = story['@graph'][0]['schema:name'];
+          $scope.presentStory.image = story['@graph'][0]['schema:image'] ||
+                                      story['@graph'][0]['schema:logo'];
+        }
+      });
+    }
+
+    // Get the appropriate image given the video demographics
+    function getVideoPresenceImage(event) {
+      if(event.glasses === 'no') {
+        switch(event.mood) {
+          case 'unhappy':
+            return 'images/unhappy.jpg';
+          case 'neutral':
+            return 'images/neutral.jpg';
+          case 'happy':
+            return 'images/happy.jpg';
+          case 'very_happy':
+            return 'images/veryhappy.jpg';
+        }
+      }
+      else {
+        switch(event.mood) {
+          case 'unhappy':
+            return 'images/unhappy-glasses.jpg';
+          case 'neutral':
+            return 'images/neutral-glasses.jpg';
+          case 'happy':
+            return 'images/happy-glasses.jpg';
+          case 'very_happy':
+            return 'images/veryhappy-glasses.jpg';
+        }
+      }
     }
 
   });
